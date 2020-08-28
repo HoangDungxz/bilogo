dbe98752d8e92a022b19c3bdc00c7c94b016f845
HoGib98 <gibhud98@gmail.com>
2020-08-28 11:19:07 +0700

    28/08 by Ho

--- requirejs-config.js
+++ requirejs-config.js
@@ -14,7 +14,10 @@ var config = {
  map: {
     '*': {
         qtyInput:               'js/widgets/qty-input',
-        customCalendar:         'js/widgets/custom-calendar'
+        customCalendar:         'js/widgets/custom-calendar',
+        // 'Magento_Checkout/template/minicart/item/default.html': 'template/minicart/item/default.html',
+        'sidebar': 'js/sidebar',
+        'Magento_Checkout/js/view/minicart': 'js/view/minicart'
     }
 }
 };
